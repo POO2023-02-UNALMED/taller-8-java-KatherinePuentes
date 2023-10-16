c7c8b22b8d71c0094cd809b8155726d133ddea75
KatherinePuentes <141962369+KatherinePuentes@users.noreply.github.com>
2023-10-16 01:39:02 +0000

    cambios

--- src/main/java/futbol/Futbolista.java
+++ src/main/java/futbol/Futbolista.java
@@ -1,6 +1,6 @@
 package futbol;
 
-public abstract class Futbolista implements Comparable<Futbolista>{
+public abstract class Futbolista implements Comparable{
     private String nombre;
     private int edad;
     private final String posicion;

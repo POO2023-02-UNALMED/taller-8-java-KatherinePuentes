5ab18b59bd82ddf0eeec6ca9a242b1777acabd18
KatherinePuentes <141962369+KatherinePuentes@users.noreply.github.com>
2023-10-16 00:44:34 +0000

    clase futbolista

--- src/main/java/Futbolista.java
+++ src/main/java/Futbolista.java
@@ -1,3 +1,45 @@
-public abstract class Futbolista{
-    
+public abstract class Futbolista implements Comparable<Futbolista>{
+    private String nombre;
+    private int edad;
+    private final String posicion;
+
+    public Futbolista(String nombre, int edad, String posicion){
+        this.nombre= nombre;
+        this.edad= edad;
+        this.posicion= posicion;
+    }
+
+    public Futbolista(){
+        this("Maradona", 30, "delantero");
+    }
+
+    public String toString(){
+        return String.format("El futbolista %s tiene %d, y juega de %s", nombre, edad, posicion);
+    }
+
+    public boolean equals(Futbolista f){
+        return f==this;
+    }
+
+    public abstract boolean jugarConLasManos();
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre= nombre;
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
+    public void setEdad(int edad){
+        this.edad= edad;
+    }
+
+    public String getPosicion(){
+        return posicion;
+    }
 }

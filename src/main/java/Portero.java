6b0f3290756282f16a200e880004dabaf4585e81
KatherinePuentes <141962369+KatherinePuentes@users.noreply.github.com>
2023-10-16 01:31:00 +0000

    clase portero

---
+++ src/main/java/Portero.java
@@ -0,0 +1,26 @@
+public class Portero extends Futbolista{
+    public short golesRecibidos;
+    public byte dorsal;
+
+    public Portero(String nombre, int edad, String posicion, short golesRecibidos, byte dorsal){
+        super(nombre, edad, "Portero");
+        this.golesRecibidos= golesRecibidos;
+        this.dorsal= dorsal;
+    }
+
+    @Override
+    public boolean jugarConLasManos(){
+        return true;
+    }
+
+    public String toString(){
+        String texto1= super.toString();
+        return String.format("%s con el dorsal %d. Le han marcado %d", texto1, dorsal, golesRecibidos);
+    }
+
+    @Override
+    public int compareTo(Portero otroPortero){
+        int diferenciaGoles= this.golesRecibidos - otroPortero.golesRecibidos;
+        return Math.abs(diferenciaGoles);
+    }
+}
